0f91a3ba02bfb8283cbe0b1a72202af3307f52e9
Victory <34212112+VictoryCoon@users.noreply.github.com>
2024-07-20 20:37:22 +0900

    Update Memo.sql

--- Memo.sql
+++ Memo.sql
@@ -1,3 +1,6 @@
+[VM]
+https://github.com/utmapp/UTM/releases
+
 [MariaDB]
 $> docker pull mariadb
 $> docker run —detach —name mariadb -p 3306:3306 —env MARIADB_USER=kspot —env MARIADB_PASSWORD=${PASSWORD} —env MARIADB_ROOT_PASSWORD=oo1414036! mariadb:latest
